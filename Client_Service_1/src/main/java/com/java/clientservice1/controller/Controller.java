445b08c7395c399e11817a4c7d67bf02808eb3f4
Tamal <onlyjohn98@gmail.com>
2019-08-29 01:47:33 +0530

    Rest Template chnages

--- Client_Service_1/src/main/java/com/java/clientservice1/controller/Controller.java
+++ Client_Service_1/src/main/java/com/java/clientservice1/controller/Controller.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,17 +26,26 @@ public class Controller {
 	@Autowired
 	@Qualifier(value="LoadbalancedRestTemplate")
 	private RestTemplate resttemplate;
+	
+	@Autowired
+	LoadBalancerClient client;
 
-	@RequestMapping(value="/service1/limiting/{value}", method=RequestMethod.POST)
+	@GetMapping(value="/service1/limiting/{value}")
 	public ArrayList<User> getData(@PathVariable String value) {
 		ArrayList<User> userList = new ArrayList<>();
+		ServiceInstance clientName = client.choose("Config-Client-2");
 		String url = "/service2/search/user";
+		String finalUri = clientName.getUri()+url;
 		HttpEntity<String> entity = new HttpEntity<>(value);
-		ResponseEntity<ArrayList> response = resttemplate.exchange(url, HttpMethod.POST, entity, ArrayList.class);
+		ResponseEntity<ArrayList> response = null;
+		try {
+			response = resttemplate.exchange(finalUri, HttpMethod.POST, entity, ArrayList.class);
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
 		if(null!=response) {
 			userList = response.getBody();
 		}
-		;
 		return userList;
 	}
 }
